76ddd2c4fbde5ffdc6f9378d7481db4678405f38
bakuri kokhodze <b_kokhodze2@cu.edu.ge[=]'cleargit config --global user.email b>
2020-10-31 20:07:15 +0400

    Updates

---
+++ precache-manifest.8ece98bca388bfe266e1ea9291192c31.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8beec3edb25f11e1e8983296840acde5",
+    "url": "/insta-react/index.html"
+  },
+  {
+    "revision": "c6a11760143ff9ebd626",
+    "url": "/insta-react/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "6fac3cda87ef897a42de",
+    "url": "/insta-react/static/css/main.a8becc20.chunk.css"
+  },
+  {
+    "revision": "c6a11760143ff9ebd626",
+    "url": "/insta-react/static/js/2.d6372c8e.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/insta-react/static/js/2.d6372c8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fac3cda87ef897a42de",
+    "url": "/insta-react/static/js/main.f6c1a03f.chunk.js"
+  },
+  {
+    "revision": "d86df5d12c4622e7fe41",
+    "url": "/insta-react/static/js/runtime-main.9f90715a.js"
+  },
+  {
+    "revision": "f38e0c36b0aee6031aa9039b28858025",
+    "url": "/insta-react/static/media/1.f38e0c36.jpg"
+  },
+  {
+    "revision": "a2d3a84a86c4b470c15857579d790e54",
+    "url": "/insta-react/static/media/2.a2d3a84a.jpg"
+  },
+  {
+    "revision": "105f2961d011cd21d5d224aec62139bc",
+    "url": "/insta-react/static/media/3.105f2961.jpg"
+  },
+  {
+    "revision": "ded916de1ba7ca5ac3a0b24f5bdcd399",
+    "url": "/insta-react/static/media/4.ded916de.jpg"
+  },
+  {
+    "revision": "b3d1738fe5fcf3cb24ddf9af0e182630",
+    "url": "/insta-react/static/media/5.b3d1738f.jpg"
+  },
+  {
+    "revision": "126ebd8aa820e5c7d3e1001350e0c5aa",
+    "url": "/insta-react/static/media/6.126ebd8a.jpg"
+  },
+  {
+    "revision": "cd6b9880ae9c7b791cee3a7321439ea2",
+    "url": "/insta-react/static/media/7.cd6b9880.jpg"
+  },
+  {
+    "revision": "3b8ad90a7a5366149b0595e667396d65",
+    "url": "/insta-react/static/media/error.3b8ad90a.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/insta_reactt/precache-manifest.f06267f943c671336eebffe4c4d5f854.js"
+  "/insta-react/precache-manifest.8ece98bca388bfe266e1ea9291192c31.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/insta_reactt/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/insta-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
